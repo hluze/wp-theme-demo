8c836039c70336590efabbc6726605f7669729ea
hluze <1932614@cgmatane.qc.ca>
2022-02-07 16:35:10 -0500

    Add theme files

---
+++ footer.php
@@ -0,0 +1,11 @@
+</main>
+
+    <footer class="flex v-aligne">
+        <div class="conteneur">
+            <p><?php bloginfo('title');?>, tous droits réservés &copy <?php echo (date('Y')) ?></p>
+        </div>
+
+    </footer>
+    <?php wp_footer();?>
+    </body>
+</html>
